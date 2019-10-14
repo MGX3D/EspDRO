8d9c40bfa6d9da17d880e32a76cca2be7963b544
Marius G <mariusg@live.com>
2019-10-13 23:16:47 -0700

    use mDNS name instead of hardcoded IP. Updated Python sample for async IO and json decoding

--- examples/pyDRO/pyDRO.py
+++ examples/pyDRO/pyDRO.py
@@ -1,10 +1,18 @@
-import websocket
- 
-ws = websocket.WebSocket()
-ws.connect("ws://192.168.1.8:81/")
-  
-while 1:
-    result = ws.recv()
-    print(result)
- 
-ws.close()
+#
+# Sample Python Reader for #EspDRO
+#
+import json
+import asyncio
+import websockets
+
+async def espdro_client():
+        wsc = await websockets.connect("ws://espdro:81")
+        while True:
+            reading = await wsc.recv()
+            if __debug__: print("EspDRO: {}".format(reading))
+            if reading.startswith("{"):
+                jreading = json.loads(reading)
+                print("  x={:,d}um time={:d}".format(jreading['axis0'], jreading['ts']))
+
+# Python 3.7+: asyncio.run(espdro_client())
+asyncio.get_event_loop().run_until_complete(espdro_client())
